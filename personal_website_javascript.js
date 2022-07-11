73fdb00143a9f37beeb2c214c6f1d5c81fb8468b
shlok-007 <21cs02008@iitbbs.ac.in>
2022-07-11 21:42:51 +0530

    Changes made:- 1. Fixed discord link. 2. Added transitions. 3. Added responsiveness through flexbox. 4. Other minor changes and tweaks.

--- personal_website_javascript.js
+++ personal_website_javascript.js
@@ -1,1 +1,8 @@
 alert("This website is currently under development and is also my first major Web Development Project. So, feel free to suggest any changes :-)")
+
+function copyfn(st){
+    // st.select();
+    // st.setSelectionRange(0, 99999);
+    navigator.clipboard.writeText(st);
+    alert('Username Copied!');
+}
