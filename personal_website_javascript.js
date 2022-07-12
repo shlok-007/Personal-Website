f2ec2f6ba6de8fe491dd41a2340f15fd1670699c
shlok-007 <21cs02008@iitbbs.ac.in>
2022-07-12 23:30:20 +0530

    Changes made: 1. Added responsiveness to the content section. 2. Fixed formatting issues. 3. Other minor changes and tweaks.

--- personal_website_javascript.js
+++ personal_website_javascript.js
@@ -1,4 +1,4 @@
-alert("This website is currently under development and is also my first major Web Development Project. So, feel free to suggest any changes :-)")
+// alert("This website is currently under development and is also my first major Web Development Project. So, feel free to suggest any changes :-)")
 
 function copyfn(st){
     // st.select();
